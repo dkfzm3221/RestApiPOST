be3b115e385b049734c9190aa85862e88f07792c
Kimjaeseop <dkfzm3221@naver.com>
2021-10-04 15:16:15 +0900

    2021-10-04

--- src/main/java/com/example/post/controller/PostApiController.java
+++ src/main/java/com/example/post/controller/PostApiController.java
@@ -26,8 +26,5 @@ public class PostApiController {
         System.out.println(requestData);
     }
 
-    @PostMapping("/post02")
-    public void post02(@RequestBody PostRequestDto requestData) {
-        System.out.println(requestData);
-    }
+
 }
